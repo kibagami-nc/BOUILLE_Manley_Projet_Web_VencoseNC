00ca3a2a934e712c1889aa269581edfd35281580
Manley.B <manley-bouille@outlook.fr>
2026-04-28 08:19:55 +1100

    feat(BackendDatabase): Creation of backend's structure and postgresql script

---
+++ backend/src/main/java/nc/kibagami_nc/vencosenc/VencosencApplication.java
@@ -0,0 +1,13 @@
+package nc.kibagami_nc.vencosenc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VencosencApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VencosencApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/nc/kibagami_nc/vencosenc/VencosencApplicationTests.java
@@ -0,0 +1,13 @@
+package nc.kibagami_nc.vencosenc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VencosencApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
